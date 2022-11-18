1b6958b059d29da37f086c06551a89edbdddcb7b
meisolated <isolatedbot@gmail.com>
2022-11-18 21:11:53 +0530

    removed something

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-/**
- *
- * Over here we have to make optimized version of FyersApi2 package
- * WHAT TO DO:
- * 1. Make a class FyersApi2
- * 2. How can we handle multiple instances of FyersApi2 with different tokens
- * 3. How can we handle multiple websocket connections with different tokens
- *
- *
- */
 import axios from "axios"
 import * as helper from "./helper"
 import { apiUrls } from "./helper"
